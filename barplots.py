64a096d06d9604e01ca6d1cdb19b41bf38285aa9
Faouzi <40366800+Faouzizi@users.noreply.github.com>
2023-07-05 19:07:52 +0200

    Update barplots.py

--- barplots.py
+++ barplots.py
@@ -14,3 +14,30 @@ def plot_barplot(df, x_axis, y_axis, title, vertical, rotation):
     plt.show()
 
 
+
+# Plot a barplot using different color by group of rows
+color_palette = {
+    'Struggling Stars': 'red',
+    'Developing Dynamos': 'orange',
+    'Competent Champions': 'green',
+    'Exceptional Experts': 'blue',
+    'Elite Achievers': 'purple'
+}
+def plot_barplot(df, x_axis, y_axis, title, rotation, color_palette, group_name):
+    """plot barplot with rotation xaxis"""
+    ax = sns.barplot(x=x_axis,
+                     y=y_axis,
+                     data=df,
+                     hue=group_name,
+                     palette=color_palette
+                     )
+    if rotation:
+        ax.set_xticklabels(ax.get_xticklabels(), rotation)
+    sns.despine(
+        left=True,
+        bottom=True
+    )
+    plt.title(title)
+    plt.show()
+
+
